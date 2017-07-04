4cdb23e2d27fa8d3954493464f3d4f4c33aa363d
kx121314 <kx121314@163.com>
2017-07-04 22:40:21 +0800

    Signed-off-by: kx121314 <kx121314@163.com>

--- mvc_smarty/configs/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$config = array(
-		'viewconfig' => array(
-			'left_delimiter' => '{',  'right_delimiter' => '}',  'template_dir' => 'tpl',  'compile_dir' => 'data/template_c'),
-		'dbconfig' => array('DB_TYPE'=>'mysql','DB_HOST'=>'127.0.0.1','DB_NAME'=>'final','DB_USER'=>'root','DB_PSW'=>'root')
-	);
-?>
--- mvc_smarty/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	header("Content-type: application/json;charset=utf-8");
-	//url形式  index.php?controller=控制器名&method=方法名
-	require_once('configs/config.php');
-	require_once('framework/pc.php');
-	PC::run($config);
- ?>
